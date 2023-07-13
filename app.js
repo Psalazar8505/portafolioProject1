43a6c1a7075dd48de4196b1aae97646bcdc379e9
Pablo Salazar <psalazar.ol71@gmail.com>
2023-07-13 13:05:12 -0600

    se soluciona el data set de los iconos de 'controlls'

--- app.js
+++ app.js
@@ -10,28 +10,29 @@ function pageTransitions(){
         sectBtn[i].addEventListener('click',function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn'
+            this.className += ' active-btn';
         });
     };
     //section active class
-allSections.addEventListener('click', (e)=> {
-    const id =e.target.dataset.id;
-    if(id){
-        //remove selected from other btns
-        sectBtns.forEach((btn)=>{
-            btn.classList.remove('active')
-        });
-        e.target.classList.add('active')
+    allSections.addEventListener('click', (e)=> {
+        const id =e.target.dataset.id;
+        console.log(e.target.dataset);
+        if(id){
+            //remove selected from other btns
+            sectBtns.forEach((btn)=>{
+                btn.classList.remove('active')
+            });
+            e.target.classList.add('active')
 
-        //hide other section
-        sections.forEach((section)=>{
-            section.classList.remove('active')
-        });
+            //hide other section
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            });
 
-        const element = document.getElementById(id);
-        element.classList.add('active')
-    };
-})
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        };
+    });
 };
 
 pageTransitions();
